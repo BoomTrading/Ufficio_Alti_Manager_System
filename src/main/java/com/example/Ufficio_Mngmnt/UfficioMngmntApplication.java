9877c5c93bd6be6fd5295dd688a13820141360d6
BoomTrading <orytami@gmail.com>
2025-04-02 09:59:26 +0200

    first commit

---
+++ src/main/java/com/example/Ufficio_Mngmnt/UfficioMngmntApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ufficio_Mngmnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfficioMngmntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfficioMngmntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ufficio_Mngmnt/UfficioMngmntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ufficio_Mngmnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfficioMngmntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
